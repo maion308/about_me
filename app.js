a695c13758e657f694846fbb78f7ba4d8d29d82a
Michael Ion <michael.ion.mi@outlook.com>
2019-11-26 18:03:12 -0500

    completed show hide js and css effects for about projects and contacts links to have single page static website

--- app.js
+++ app.js
@@ -4,26 +4,44 @@ const projects = document.querySelector("#project-link");
 const mainContent = document.querySelector("#main-content");
 const imgContent = document.querySelector("#img-content");
 const aboutLink = document.querySelector("#about-link");
+const contactLink = document.querySelector("#contact-link");
+const projectBox = document.querySelector("#project-content")
+const contactContent = document.querySelector(".contact-container")
 
 aboutMe.addEventListener("click", () => {
     aboutMeContainer.classList.toggle("hide")
 })
 
 projects.addEventListener("click", () => {
-    if (mainContent.classList != "hide") {
+    if (mainContent.classList != "hide" || imgContent.classList != "hide" || contactLink.classList != "hide") {
         mainContent.setAttribute("class", "hide");
         mainContent.classList.remove("main-container");
         imgContent.setAttribute("class", "hide");
         imgContent.classList.remove("image-container");
+        projectBox.classList.remove("hide");
+        contactContent.setAttribute("class", "hide");
     }
 
 })
 
-aboutLink.addEventListener("click", (e) => {
+aboutLink.addEventListener("click", () => {
     if(mainContent.classList == "hide") {
         mainContent.setAttribute("class", "main-container");
         mainContent.classList.remove("hide");
         imgContent.setAttribute("class", "image-container");
         imgContent.classList.remove("hide");  
     }
+})
+
+contactLink.addEventListener("click", () => {
+    if (mainContent.classList != "hide" || imgContent.classList != "hide" || projects.classList != "hide") {
+        mainContent.setAttribute("class", "hide");
+        mainContent.classList.remove("main-container");
+        imgContent.setAttribute("class", "hide");
+        imgContent.classList.remove("image-container");
+        projectBox.setAttribute("class", "hide")
+        projectBox.classList.remove("project-container")
+        contactContent.classList.remove("hide")
+    }
+
 })
